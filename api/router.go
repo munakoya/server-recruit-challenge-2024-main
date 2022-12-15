193169b278f4e2b888b4f6aad0babcb645f62de0
munasakuta <muna.sakasakuta@gmail.com>
2022-12-15 23:58:41 +0900

    first commit

---
+++ api/router.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/pulse227/server-recruit-challenge-sample/api/middleware"
+	"github.com/pulse227/server-recruit-challenge-sample/controller"
+	"github.com/pulse227/server-recruit-challenge-sample/infra/memorydb"
+	"github.com/pulse227/server-recruit-challenge-sample/service"
+)
+
+func NewRouter() *mux.Router {
+	singerRepo := memorydb.NewSingerRepository()
+	singerService := service.NewSingerService(singerRepo)
+	singerController := controller.NewSingerController(singerService)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/singers", singerController.GetSingerListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/singers/{id:[0-9]+}", singerController.GetSingerDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/singers", singerController.PostSingerHandler).Methods(http.MethodPost)
+	r.HandleFunc("/singers/{id:[0-9]+}", singerController.DeleteSingerHandler).Methods(http.MethodDelete)
+
+	// ここにalbumsのルーター追加される
+	r.Use(middleware.LoggingMiddleware)
+
+	return r
+}
